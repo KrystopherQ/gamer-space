c89e30376260a4de9cd8d4d3d98414d0eb5c1a92
Samuel Mandel <slman.760@gmail.com>
2022-06-07 15:10:14 -0700

    added apollo provider and fixed spelling error in server

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,30 @@ import Friends from "./Components/Friends.jsx";
 import MiniGames from "./Components/MiniGames.jsx";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import React from 'react';
+import {ApolloClient, ApolloProvider, InMemoryCache, createHttpLink} from '@apollo/client'
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+const authLink = setContext((_, {headers}) => {
+  const token = localStorage.getItem('id_token')
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    }
+  }
+})
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+})
 
 function App() {
   return (
+    <ApolloProvider client={client}>
     <Router>
       <Routes>
         <Route exact path='/feed' element={<Feed />} />
@@ -15,6 +36,7 @@ function App() {
         <Route path='/minigames' element={<MiniGames />} />
       </Routes>
     </Router>
+    </ApolloProvider>
   );
 }
 
