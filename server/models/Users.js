0f611e11ef17512ee1b309b3a4fb59bccda8d9bd
Samuel Mandel <slman.760@gmail.com>
2022-06-06 17:03:55 -0700

    added user model with bcrypt and game count

---
+++ server/models/Users.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const gameSchema = require('./Games')
+
+const userSchema = new Schema(
+    {
+      username: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Must use a valid email address'],
+      },
+      password: {
+        type: String,
+        required: true,
+      },
+      savedGames: [gameSchema],
+    },
+    {
+      toJSON: {
+        virtuals: true,
+      },
+    }
+  );
+
+  userSchema.pre('save', async function(next){
+      if(this.isNew || this.isModified('password')){
+          const saltRounds = 10;
+          this.password = await bcrypt.hash(this.password, saltRounds)
+      }
+      next();
+  })
+
+  userSchema.virtual('gameCount').get(function(){
+      return this.savedGames.length;
+  })
+
+  const User = model('User', userSchema);
+
+  module.exports = User;
