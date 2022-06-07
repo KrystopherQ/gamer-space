366122cff9b7614f69c2737ac112a968deb5c85a
Krystopher Quintero <krystopher.q@gmail.com>
2022-06-07 11:34:31 -0700

    fixed title and link bug

--- server/utils/scraper.js
+++ server/utils/scraper.js
@@ -12,16 +12,18 @@ const getPostLinks = async () => {
 		// const postTitles = [];
 $('.outbound').each( (index, value) => {
 	var link = $(value).attr('href');
-	// links.push({"link": link});
+	var title = $(value).text()
+	if(title) {
+		links.push( [title,link]);}
+	});
+// $('div > p.title>a').each((_idx,el) => {
+	// const postTitle = $(el).text()
+// 	// var link = $(value).attr('href');
+// 	links.push([postTitle, link])
+// // 	links.push({"link": link});
 // });
-$('div > p.title>a').each((_idx,el) => {
-	const postTitle = $(el).text()
-	// var link = $(value).attr('href');
-	links.push([postTitle, link])
-// 	links.push({"link": link});
-});
 
-});
+// });
 return links;
 } catch (error) {
 	throw error;
@@ -31,6 +33,7 @@ getPostLinks()
 .then((links) => console.log(links));
 
 
+
 const getPostNames = async () => {
 	try {
 		const { data } = await axios.get(
@@ -38,16 +41,18 @@ const getPostNames = async () => {
 		);
 		const $ = cheerio.load(data);
 		const PostNames = [];
-		$('div > p.title > a').each((_idx, el,value) => {
-			const postTitle = $(el).text()
-			var link = $(value).attr('href');
-			// PostNames.push(postTitle,{"link": link})
+		// $('div > p.title > a').each((_idx, el) => {
+		// 	const postTitle = $(el).text()
+		// 	var link = $(value).attr('href');
+		// 	PostNames.push(postTitle,{"link": link})
+		// });
 		
-    $('.outbound').each( (index, value) => {
-			var link = $(value).attr('href');
-			PostNames.push([postTitle,link]);
-		});
-	});
+		$('.outbound').each( (index, value) => {
+					var link = $(value).attr('href');
+			    var postTitle = $(value).text()
+			if(link){
+					PostNames.push([postTitle,link]);}
+				});
 		return PostNames;
 	} catch (error) {
 		throw error;
@@ -55,3 +60,5 @@ const getPostNames = async () => {
 };
 getPostNames()
 .then((PostNames) => console.log(PostNames));
+
+
