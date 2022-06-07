fef505d392a777f3eac65ddd3ca6f6e9c748e6b8
Krystopher Quintero <krystopher.q@gmail.com>
2022-06-06 20:07:35 -0700

    create bot to scrape r/games post titles

--- server/utils/scraper.js
+++ server/utils/scraper.js
@@ -24,3 +24,21 @@ const getPostTitles = async () => {
 getPostTitles()
 .then((postTitles) => console.log(postTitles));
 
+const getPostNames = async () => {
+	try {
+		const { data } = await axios.get(
+			'https://old.reddit.com/r/games/'
+		);
+		const $ = cheerio.load(data);
+		const PostNames = [];
+		$('div > p.title > a').each((_idx, el) => {
+			const postTitle = $(el).text()
+			PostNames.push(postTitle)
+		});
+		return PostNames;
+	} catch (error) {
+		throw error;
+	}
+};
+getPostNames()
+.then((PostNames) => console.log(PostNames));
