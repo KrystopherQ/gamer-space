1cdd1a47e1193a8a3290bacc8c7ab1905e2dac1f
KuyaDevin <devinnguyen@ucsb.edu>
2022-06-06 21:20:31 -0700

    Added navbar to friends.js

--- client/src/components/Friends.js
+++ client/src/components/Friends.js
@@ -12,6 +12,34 @@ const Friends() {
         <div>
           <img src={Logo} alt="Logo" style={{ width: '200px' }} />
         </div>
+
+        {/*navbar */}
+        <ul className='hidden md:flex'>
+          <li class="btn btn-ghost bg-neutral normal-case text-xl" >
+            <Link to='/feed'>
+              Feed
+            </Link>
+          </li>
+          <li class="btn btn-ghost normal-case text-xl">
+            <Link to='/profile'>
+              Profile
+            </Link>
+          </li>
+          <li class="btn btn-ghost normal-case text-xl">
+            <Link to='/friends'>
+              Friends
+            </Link>
+          </li>
+          <li class="btn btn-ghost normal-case text-xl">
+            <Link to='/minigames'>
+              Mini Games
+            </Link>
+          </li>
+          <div class="form-control">
+            <input type="text" placeholder="Search" class="input input-bordered" />
+          </div>
+        </ul>
+
       </div>
     </div>
   )
