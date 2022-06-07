2234d8110a7b85e31d0a2e0d769c9207293ec31b
KuyaDevin <devinnguyen@ucsb.edu>
2022-06-06 20:47:28 -0700

    Add logo to nav bar

--- client/src/components/Feed.js
+++ client/src/components/Feed.js
@@ -6,4 +6,13 @@ import { Link } from 'react-router-dom';
 const Feed = () => {
   const [nav, setNav] = useState(false)
   const handleClick = () => setNav(!nav)
+  return (
+    <div>
+      <div className='fixed w-full h-[80px] flex justify-between items-center px-4 text-gray-300 font-serif '>
+        <div>
+          <img src={Logo} alt="Logo" style={{ width: '200px' }} />
+        </div>
+      </div>
+    </div>
+  )
 }
