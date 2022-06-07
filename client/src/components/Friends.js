ffe317918aadb38510fc2e5c61b32ac04f1c8f66
KuyaDevin <devinnguyen@ucsb.edu>
2022-06-06 21:18:26 -0700

    Added logo to friends.js

--- client/src/components/Friends.js
+++ client/src/components/Friends.js
@@ -6,5 +6,14 @@ import { Link } from 'react-router-dom';
 const Friends() {
   const [nav, setNav] = useState(false)
   const handleClick = () => setNav(!nav)
+  return (
+    <div>
+      <div className='fixed w-full h-[80px] flex justify-between items-center px-4 text-gray-300 font-serif '>
+        <div>
+          <img src={Logo} alt="Logo" style={{ width: '200px' }} />
+        </div>
+      </div>
+    </div>
+  )
 }
 
