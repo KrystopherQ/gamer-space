457cab3f8ae0110a456423c9782b2da4eb94f059
Krystopher Quintero <krystopher.q@gmail.com>
2022-06-07 15:03:48 -0700

    minor change

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Feed from "./Components/Feed.jsx";
 import Profile from "./Components/Profile.jsx";
 import Friends from "./Components/Friends.jsx";
 import MiniGames from "./Components/MiniGames.jsx";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import React from 'react';
 
 function App() {
   return (
-    <Router>
+    <Router basename='/'>
       <Routes>
-        <Route exact path='/feed' element={<Feed />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='/friends' element={<Friends />} />
-        <Route path='/minigames' element={<MiniGames />} />
+        <Route path='/feed' element={<Feed/>} />
+        <Route path='/profile' element={<Profile/>} />
+        <Route path='/friends' element={<Friends/>} />
+        <Route path='/minigames' element={<MiniGames/>} />
       </Routes>
     </Router>
   );
