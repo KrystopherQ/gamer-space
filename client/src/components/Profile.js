dc452ec384794e02a33c7569bfb39a91f5dfa609
KuyaDevin <devinnguyen@ucsb.edu>
2022-06-06 21:55:41 -0700

    Add navbar to profile.js

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -13,6 +13,32 @@ function Profile() {
           <div>
             <img src={Logo} alt="Logo" style={{ width: '200px' }} />
           </div>
+          <ul className='hidden md:flex'>
+            <li class="btn btn-ghost normal-case text-xl" >
+              <Link to='/feed'>
+                Feed
+              </Link>
+            </li>
+            <li class="btn btn-ghost bg-neutral normal-case text-xl">
+              <Link to='/profile'>
+                Profile
+              </Link>
+            </li>
+            <li class="btn btn-ghost normal-case text-xl">
+              <Link to='/friends'>
+                Friends
+              </Link>
+            </li>
+            <li class="btn btn-ghost normal-case text-xl">
+              <Link to='/minigames'>
+                Mini Games
+              </Link>
+            </li>
+            <div class="form-control">
+              <input type="text" placeholder="Search" class="input input-bordered" />
+            </div>
+          </ul>
+
         </div>
       </div>
     </div>
