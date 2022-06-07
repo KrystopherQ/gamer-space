dbd1ba067cd2c1429dfb9b9be97d0ec236255ba8
KuyaDevin <devinnguyen@ucsb.edu>
2022-06-07 11:02:27 -0700

    Add minigames function

--- client/src/components/MiniGames.js
+++ client/src/components/MiniGames.js
@@ -2,3 +2,7 @@ import React, { useState } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import Logo from '../assets/Gamer Space-logos_transparent.png';
 import { Link } from 'react-router-dom';
+
+function MiniGames() {
+
+}
