25bc95337de6ef3217326e68a5ef71327e2cd7a1
Krystopher Quintero <krystopher.q@gmail.com>
2022-06-09 11:20:34 -0700

    move bot to client side

--- server/utils/scraper.js
+++ server/utils/scraper.js
@@ -1,67 +1,67 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
+// const axios = require('axios');
+// const cheerio = require('cheerio');
 
-export function bot(){
+// function bot(){
 
-const getPostLinks = async () => {
-	try {
-		const { data } = await axios.get(
-			'https://old.reddit.com/r/gamingnews/'
-		);
-		const $ = cheerio.load(data);
-		const links = [];
-		// const postTitles = [];
-$('.outbound').each( (index, value) => {
-	var link = $(value).attr('href');
-	var title = $(value).text()
-	if(title) {
-		links.push({title,link});}
-	});
-// $('div > p.title>a').each((_idx,el) => {
-	// const postTitle = $(el).text()
-// 	// var link = $(value).attr('href');
-// 	links.push([postTitle, link])
-// // 	links.push({"link": link});
-// });
+// const getPostLinks = async () => {
+// 	try {
+// 		const { data } = await axios.get(
+// 			'https://old.reddit.com/r/gamingnews/'
+// 		);
+// 		const $ = cheerio.load(data);
+// 		const links = [];
+// 		// const postTitles = [];
+// $('.outbound').each( (index, value) => {
+// 	var link = $(value).attr('href');
+// 	var title = $(value).text()
+// 	if(title) {
+// 		links.push({title,link});}
+// 	});
+// // $('div > p.title>a').each((_idx,el) => {
+// 	// const postTitle = $(el).text()
+// // 	// var link = $(value).attr('href');
+// // 	links.push([postTitle, link])
+// // // 	links.push({"link": link});
+// // });
 
-// });
-return links;
-} catch (error) {
-	throw error;
-}
-}
-getPostLinks()
-.then((links) => console.log(links));
+// // });
+// return links;
+// } catch (error) {
+// 	throw error;
+// }
+// }
+// getPostLinks()
+// .then((links) => console.log(links));
 
 
 
-const getPostNames = async () => {
-	try {
-		const { data } = await axios.get(
-			'https://old.reddit.com/r/games/'
-		);
-		const $ = cheerio.load(data);
-		const PostNames = [];
-		// $('div > p.title > a').each((_idx, el) => {
-		// 	const postTitle = $(el).text()
-		// 	var link = $(value).attr('href');
-		// 	PostNames.push(postTitle,{"link": link})
-		// });
+// const getPostNames = async () => {
+// 	try {
+// 		const { data } = await axios.get(
+// 			'https://old.reddit.com/r/games/'
+// 		);
+// 		const $ = cheerio.load(data);
+// 		const PostNames = [];
+// 		// $('div > p.title > a').each((_idx, el) => {
+// 		// 	const postTitle = $(el).text()
+// 		// 	var link = $(value).attr('href');
+// 		// 	PostNames.push(postTitle,{"link": link})
+// 		// });
 		
-		$('.outbound').each( (index, value) => {
-					var link = $(value).attr('href');
-			    var postTitle = $(value).text()
-			if(link){
-					PostNames.push({postTitle,link});}
-				});
-		return PostNames;
-	} catch (error) {
-		throw error;
-	}
-};
-getPostNames()
-.then((PostNames) => console.log(PostNames));
+// 		$('.outbound').each( (index, value) => {
+// 					var link = $(value).attr('href');
+// 			    var postTitle = $(value).text()
+// 			if(link){
+// 					PostNames.push({postTitle,link});}
+// 				});
+// 		return PostNames;
+// 	} catch (error) {
+// 		throw error;
+// 	}
+// };
+// getPostNames()
+// .then((PostNames) => console.log(PostNames));
 
-}
+// }
 
-bot();
+// bot();
