9d947b074788cf0f877d8feb439dde97adc27d79
Samuel Mandel <slman.760@gmail.com>
2022-06-08 17:21:54 -0700

    modification to app.js for portal to be the landing page for user Auth

--- client/src/components/Portal.jsx
+++ client/src/components/Portal.jsx
@@ -1,10 +1,10 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import {signUpForm} from '../utils/signUpForm'
-import {loginForm} from '../utils/loginForm'
-import Auth from '../utils'
+import { signUpForm } from '../utils/signUpForm'
+import { loginForm } from '../utils/loginForm'
+import Auth from '../utils/auth'
 
-const portal = () => {
+const Portal = () => {
     // useStates
 
     return (
@@ -12,17 +12,26 @@ const portal = () => {
             {Auth.loggedIn() ? (
                 // render pages 
                 <>
-                    
 
+                        
                 </>
             ) : (
-                
                 //render login Hero
                 <>
-                
+                    <div class="hero min-h-screen bg-base-200">
+                        <div class="hero-content text-center">
+                            <div class="max-w-md">
+                                <h1 class="text-5xl font-bold">Hello there</h1>
+                                <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                                <button class="btn btn-primary">Get Started</button>
+                            </div>
+                        </div>
+                    </div>
+
                 </>
             )}
         </>
 
     )
 }
+export default Portal
