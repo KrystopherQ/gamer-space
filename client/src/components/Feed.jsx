c88a3c77bdaa78cd8f551635c4416db432a51275
Samuel Mandel <slman.760@gmail.com>
2022-06-08 17:11:51 -0700

    add portal start, add login and signup form js for implementing auth for portal to render website navigability. Comment in Feed on bot import to prevent compile error

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import Logo from '../assets/Gamer Space-logos_transparent.png';
 import { Link } from 'react-router-dom';
-import {bot} from '../utils/scraper'
+//import {bot} from '../utils/scraper'
 
 const Feed = () => {
   const [nav, setNav] = useState(false)
---
+++ client/src/components/Portal.jsx
@@ -0,0 +1,27 @@
+import React, {useState} from "react";
+import { Link } from "react-router-dom";
+import {signUpForm} from '../utils/signUpForm'
+import {loginForm} from '../utils/loginForm'
+import Auth from '../utils'
+
+const portal = () => {
+    // useStates
+
+    return (
+        <>
+            {Auth.loggedIn() ? (
+                //render login Hero
+                <>
+                    
+
+                </>
+            ) : (
+                // render pages 
+                <>
+                
+                </>
+            )}
+        </>
+
+    )
+}
