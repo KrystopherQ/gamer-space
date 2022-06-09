07ce3c17b3299e7d43ada820fb80b27c5f3120bf
Krystopher Quintero <krystopher.q@gmail.com>
2022-06-08 18:44:03 -0700

    fix news models

---
+++ server/models/News.js
@@ -0,0 +1,12 @@
+const { Schema } = require('mongoose');
+
+const  newsSchema = new Schema( {
+    links: {
+        type: String,
+        required: true,
+    },
+    PostNames: {
+        type: String,
+    }
+})
+module.exports = newsSchema;
