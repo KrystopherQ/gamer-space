809bd3c6fa7ca0351ab5ce7eb07ffbc7cb1bbade
KuyaDevin <devinnguyen@ucsb.edu>
2022-06-09 11:51:11 -0700

    fix title on friends.js

--- client/src/components/Friends.jsx
+++ client/src/components/Friends.jsx
@@ -71,7 +71,7 @@ function Friends() {
       </div>
       <div className='w-full h-80'>
         <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full text-white font-serif grid-cols-2'>
-          <h1 className='text-4xl'>Feed</h1>
+          <h1 className='text-4xl'>Friends</h1>
           <div className='flex grid-cols-2 gap-4'>
             <div className="card w-96 bg-neutral shadow-2xl">
               <div className="card-body">
