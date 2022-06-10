d5e84ed8faaa34f0dc65054d77e57f826019c0f0
KuyaDevin <devinnguyen@ucsb.edu>
2022-06-10 13:15:31 -0700

    Change class to className on profile.js

--- client/src/Components/Feed.jsx
+++ client/src/Components/Feed.jsx
@@ -1,101 +1,77 @@
-import React, { useState } from 'react';
-import { FaBars, FaTimes } from 'react-icons/fa';
-import Logo from '../assets/Gamer Space-logos_transparent.png';
-import { Link } from 'react-router-dom';
-<<<<<<< HEAD
-<<<<<<< HEAD
-// import {bot} from '../utils/scraper'
-// console.log(bot)
-=======
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import Logo from "../assets/Gamer Space-logos_transparent.png";
+import { Link } from "react-router-dom";
 
-<<<<<<< HEAD
->>>>>>> 37f98795407383ff93d6065468d447171858b103
+function Feed() {
+	const [nav, setNav] = useState(false);
+	const handleClick = () => setNav(!nav);
 
-=======
-=======
-//None of these work
-// import {bot} from '../utils/scraper'
-// import {runAPI} from '../utils/api'
-// breaks
-//console.log(runAPI)
-//returns undefined
-//console.log(bot)
->>>>>>> ef533459c51377eddf03f15414fa3d94c6f439b2
-// page for news scrapper
->>>>>>> da8666edac771d6b79ba50c6958040825ed49c18
-const Feed = () => {
-  const [nav, setNav] = useState(false)
-  const handleClick = () => setNav(!nav)
-  
-  return (
-    <div>
-      <div className='fixed w-full h-[80px] flex justify-between items-center px-4 text-gray-300 font-serif '>
-        <div>
-          <img src={Logo} alt="Logo" style={{ width: '200px' }} />
-        </div>
+	return (
+		<div>
+			<div className="fixed w-full h-[80px] flex justify-between items-center px-4 text-gray-300 font-serif ">
+				<div>
+					<img src={Logo} alt="Logo" style={{ width: "200px" }} />
+				</div>
 
-        {/*navbar */}
-        <ul className='hidden md:flex'>
-          <li className="btn btn-ghost bg-neutral normal-case text-xl" >
-            <Link to='/feed'>
-              Feed
-            </Link>
-          </li>
-          <li className="btn btn-ghost normal-case text-xl">
-            <Link to='/profile'>
-              Profile
-            </Link>
-          </li>
-          <li className="btn btn-ghost normal-case text-xl">
-            <Link to='/friends'>
-              Friends
-            </Link>
-          </li>
-          <li className="btn btn-ghost normal-case text-xl">
-            <Link to='/minigames'>
-              Mini Games
-            </Link>
-          </li>
-          <div className="form-control">
-            <input type="text" placeholder="Search" className="input input-bordered" />
-          </div>
-        </ul>
+				{/*navbar */}
+				<ul className="hidden md:flex">
+					<li className="btn btn-ghost bg-neutral normal-case text-xl">
+						<Link to="/feed">Feed</Link>
+					</li>
+					<li className="btn btn-ghost normal-case text-xl">
+						<Link to="/profile">Profile</Link>
+					</li>
+					<li className="btn btn-ghost normal-case text-xl">
+						<Link to="/friends">Friends</Link>
+					</li>
+					<li className="btn btn-ghost normal-case text-xl">
+						<Link to="/minigames">Mini Games</Link>
+					</li>
+					<div className="form-control">
+						<input
+							type="text"
+							placeholder="Search"
+							className="input input-bordered"
+						/>
+					</div>
+				</ul>
 
-        {/*Hamburger */}
-        <div onClick={handleClick} className='md:hidden z-10'>
-          {!nav ? <FaBars /> : <FaTimes />}
-        </div>
+				{/*Hamburger */}
+				<div onClick={handleClick} className="md:hidden z-10">
+					{!nav ? <FaBars /> : <FaTimes />}
+				</div>
 
-        {/*Mobile*/}
-        <div>
-          <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#556b2f] flex flex-col justify-center items-center'}>
-            <li className='py-6 text-4xl'>
-              <Link to='/feed'>
-                Feed
-              </Link></li>
-            <li className='py-6 text-4xl'>
-              <Link to='/profile'>
-                Profile
-              </Link></li>
-            <li className='py-6 text-4xl'>
-              <Link to='/friends'>
-                Friends
-              </Link>
-            </li>
-            <li className='py-6 text-4xl'>
-              <Link to='/minigames'>
-                Mini Games
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div className='w-full h-80'>
-        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full text-white font-serif grid-cols-2'>
-          <h1 className='text-4xl'>Feed</h1>
-          <div className='flex grid-cols-2 gap-4'>
-                            {/* Gaming News */}                            
-              {/* {bot.map((newsFeed) => {
+				{/*Mobile*/}
+				<div>
+					<ul
+						className={
+							!nav
+								? "hidden"
+								: "absolute top-0 left-0 w-full h-screen bg-[#556b2f] flex flex-col justify-center items-center"
+						}
+					>
+						<li className="py-6 text-4xl">
+							<Link to="/feed">Feed</Link>
+						</li>
+						<li className="py-6 text-4xl">
+							<Link to="/profile">Profile</Link>
+						</li>
+						<li className="py-6 text-4xl">
+							<Link to="/friends">Friends</Link>
+						</li>
+						<li className="py-6 text-4xl">
+							<Link to="/minigames">Mini Games</Link>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div className="w-full h-80">
+				<div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full text-white font-serif grid-cols-2">
+					<h1 className="text-4xl">Feed</h1>
+					<div className="flex grid-cols-2 gap-4">
+						{/* Gaming News */}
+						{/* {bot.map((newsFeed) => {
                 console.log(bot)
                   return (
                 <div className="card w-96 bg-neutral shadow-2xl">
@@ -106,18 +82,18 @@ const Feed = () => {
                 </div>
                   );
                 })} */}
-            <div className="card w-96 bg-primary shadow-2xl">
-              <div className="card-body">
-                <h2 className="card-title">Top Gaming News</h2>
-                {/* Twitch Games Being played */}
-                <p>Enter Gaming News Here</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+						<div className="card w-96 bg-primary shadow-2xl">
+							<div className="card-body">
+								<h2 className="card-title">Top Gaming News</h2>
+								{/* Twitch Games Being played */}
+								<p>Enter Gaming News Here</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Feed;
