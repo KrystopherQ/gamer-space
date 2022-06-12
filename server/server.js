c3ea593928c7bfc82aba4698713e2b59474bbb5f
Krystopher Quintero <krystopher.q@gmail.com>
2022-06-11 21:12:45 -0700

    create routes for my bot

--- server/server.js
+++ server/server.js
@@ -4,6 +4,10 @@ const path = require('path')
 const {typeDefs, resolvers} = require('./schemas')
 const db = require('./config/connection')
 const {authMiddleware} = require('./utils/auth');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const scraper = require('./utils/scraper')
+
 
 const app = express();
 const PORT = process.env.PORT || 3001
@@ -16,7 +20,8 @@ const server = new ApolloServer({
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json())
-///
+/////-------------------------------------games--------------------------------------
+
 require('dotenv').config();
 const request = require('request')
 //function runAPI() {
@@ -71,9 +76,73 @@ const getGames = (res, url,accessToken,callback) => {
         //console.log(gameInfo.data[0].name)   
     })
 }
-// }
-///
-//for production build
+//-------------------------------------games--------------------------------------
+
+
+///------------------------------------------------------Scraper---------------------------------------------------------
+
+app.get('/api/gamenews', (req,res)=>{
+    getGamingNews()
+    .then((gamingNews) =>{ 
+    console.log(gamingNews)
+        res.json(gamingNews)
+});    
+})
+
+app.get('/api/news', (req,res)=>{
+    getRGames()
+    .then((games) => {
+    console.log(games)
+    res.json(games)
+
+});
+})
+
+
+const getGamingNews = async () => {
+	try {
+		const { data } = await axios.get(
+			'https://old.reddit.com/r/gamingnews/'
+		);
+		const $ = cheerio.load(data);
+		const gamingNews = [];
+$('.outbound').each( (index, value) => {
+	var link = $(value).attr('href');
+	var title = $(value).text()
+	if(title) {
+		gamingNews.push({title,link});}
+	});
+return gamingNews;
+} catch (error) {
+	throw error;
+}
+}
+
+
+const getRGames = async () => {
+	try {
+		const { data } = await axios.get(
+			'https://old.reddit.com/r/games/'
+		);
+		const $ = cheerio.load(data);
+		const games = [];		
+		$('.outbound').each( (index, value) => {
+					var link = $(value).attr('href');
+			    var title = $(value).text()
+			if(link){
+					games.push({title,link});}
+				});
+		return games;
+	} catch (error) {
+		throw error;
+	}
+};
+
+
+///------------------------------------------------------Scraper---------------------------------------------------------
+
+
+//-----------------------------------------for production build----------------------------------------------------------
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')))
 } 
