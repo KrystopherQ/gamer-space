7ae1cc4556eaebbdb214f78349d449cc05ca1c27
Samuel Mandel <samuelmandel@Samuels-MacBook-Pro.local>
2022-06-12 15:04:12 -0700

    search users handling input and form submit for user search

--- client/src/utils/searchUsers.js
+++ client/src/utils/searchUsers.js
@@ -1,2 +1,34 @@
 import react, {useState} from 'react'
-import 
+import { useQuery } from '@apollo/client'
+import { SEARCH_USER } from './queries'
+
+
+const searchUser= () => {
+    const [searchForm, setSearchForm] = useState({username: ''})
+    const [search] = useQuery(SEARCH_USER)
+    const handleInputChange = (event) => {
+        const {name, value} = event.target;
+        setSearchForm({...searchForm, [name]: value})
+    }
+
+    const handleFormSubmit= async (event) => {
+        event.preventDefault();
+
+
+        try {
+
+            const {data} = await search({
+                variables: {...userFormData}
+            });
+
+        } catch (err) {
+            console.error(err);
+        }
+        setSearchForm({
+            username: '',
+        })
+    }
+
+    return ()
+}
+export default searchUsers;
