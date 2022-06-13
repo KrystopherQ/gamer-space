0d940c7a499a48d17164f6dba9bf7226789ff868
KuyaDevin <devinnguyen@ucsb.edu>
2022-06-13 10:03:45 -0700

    Fixed vertical responsiveness

--- client/src/Components/Feed.jsx
+++ client/src/Components/Feed.jsx
@@ -74,8 +74,8 @@ function Feed() {
 			</div>
 			<div className={nav ? "hidden" : "w-full h-80"}>
 				<div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full text-white  font-serif grid-cols-2">
-					<h1 className="text-4xl pb-8 z-40">Feed</h1>
-					<div className=" absolute bottom-0 flex flex-col grid-row-2 h-4/5">
+					<div className=" fixed bottom-0 flex flex-col grid-row-2 h-4/5">
+						<h1 className="text-4xl pb-8 z-40">Feed</h1>
 						{/* Gaming News */}
 						{bot.map((newsFeed) => {
 							// console.log(bot);
--- client/src/Components/Friends.jsx
+++ client/src/Components/Friends.jsx
@@ -97,10 +97,10 @@ function Friends() {
 			</div>
 			<div className={nav ? "hidden" : "w-full h-80"}>
 				<div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full text-white  font-serif grid-cols-2">
-					<h1 className="text-4xl pb-8 z-40">
-						Friend Search Results
-					</h1>
 					<div className=" absolute bottom-0 flex flex-col grid-row-2 h-4/5">
+						<h1 className="text-4xl pb-8 z-40">
+							Friend Search Results
+						</h1>
 						<div>
 							<table className=" m-1 max-w-[1000px] table table-compact w-full">
 								<tbody>
