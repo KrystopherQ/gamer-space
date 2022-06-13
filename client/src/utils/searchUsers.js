02074955bb25c346c4ebe720d471412f631f7631
KuyaDevin <devinnguyen@ucsb.edu>
2022-06-12 17:47:21 -0700

    Adjusted search bar position

--- client/src/utils/searchUsers.js
+++ client/src/utils/searchUsers.js
@@ -1,15 +1,15 @@
-import react, {useState} from 'react'
+import react, { useState } from 'react'
 import { useQuery } from '@apollo/client'
 import { SEARCH_USER } from './queries'
 
 
-const SearchUser= ({handleInputChange, handleFormSubmit, searchForm}) => {
-   
+const SearchUser = ({ handleInputChange, handleFormSubmit, searchForm }) => {
+
 
     return (
         <form onSubmit={handleFormSubmit}>
-            <input type="text" name="username" placeholder="search friends" onChange={handleInputChange} defaultValue={searchForm.username}/>
-            <button className="btn btn-square btn-outline font-serif"></button>
+            <input type="text" name="username" placeholder="Search Friends" onChange={handleInputChange} defaultValue={searchForm.username} />
+            <button className="btn btn-md btn-square btn-outline font-serif">Go!</button>
         </form>
     )
 }
